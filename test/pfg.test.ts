1c5e15e4db713a96311a6b68a12855f5c03c2999
gpspelle <gpsunicamp016@gmail.com>
2022-03-30 10:53:43 -0300

    Initial commit

---
+++ test/pfg.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Pfg from '../lib/pfg-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pfg-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Pfg.PfgStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
