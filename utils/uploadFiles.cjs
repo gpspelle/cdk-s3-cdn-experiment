7a8348f630ab1dcda91b9ce24d7a107fb9dbd1d7
gpspelle <gpsunicamp016@gmail.com>
2022-04-30 16:16:04 -0300

    code that fetches data from the s3 bucket and measure time

---
+++ utils/uploadFiles.cjs
@@ -0,0 +1,42 @@
+const path = require('path');
+const fs = require('fs');
+const S3 = require("aws-sdk/clients/s3")
+const { 
+  AWS_REGION,
+  AWS_BUCKET_NAME,
+} = process.env;
+
+const S3Client = new S3({
+    region: AWS_REGION,
+});
+
+const uploadFile = async (S3Client, filepath, filename) => {
+    const fileContent = fs.readFileSync(filepath);
+
+    const params = {
+        Bucket: AWS_BUCKET_NAME,
+        Key: filename,
+        Body: fileContent
+    };
+    
+    try {
+        await S3Client.upload(params).promise();
+        console.log("Successfuly uploaded file", filepath);
+    } catch (error) {
+        console.log(error)
+        console.error(`Error uploading file ${filepath} to bucket ${AWS_BUCKET_NAME}`);
+    }
+}
+
+const directoryPath = path.join(__dirname, 'files');
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.error('Unable to scan directory: ' + err);
+    } 
+    
+    files.forEach(function (file) {
+        filepath = directoryPath + "/" + file
+        console.log("Attempt to upload file", filepath);
+        uploadFile(S3Client, filepath, file);
+    });
+});
