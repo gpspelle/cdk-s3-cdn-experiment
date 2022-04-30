59825c5447e41e66da0bba5bc955f5c915856aee
gpspelle <gpsunicamp016@gmail.com>
2022-04-30 13:29:52 -0300

    create s3 bucket with customizable name

--- bin/pfg.ts
+++ bin/pfg.ts
@@ -6,4 +6,5 @@ import { PfgStack } from '../lib/pfg-stack';
 const app = new cdk.App();
 new PfgStack(app, 'PfgStack', {
   env: { account: '593593438961', region: 'sa-east-1' },
+  bucketName: "sa-east-1-latency-test-pfg-unicamp",
 });
--- lib/pfg-stack.ts
+++ lib/pfg-stack.ts
@@ -1,8 +1,53 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import {
+  Stack,
+  StackProps,
+  aws_s3 as s3,
+  RemovalPolicy,
+  Duration 
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+interface CustomizableStack extends StackProps {
+  bucketName: string;
+}
+
 export class PfgStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: CustomizableStack) {
     super(scope, id, props);
+
+    const BUCKET_NAME = props?.bucketName as string;
+
+    // 👇 create S3 bucket
+    const bucket = new s3.Bucket(this, "s3-bucket", {
+      bucketName: BUCKET_NAME,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: false,
+      publicReadAccess: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      cors: [
+        {
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.POST,
+            s3.HttpMethods.PUT,
+          ],
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"],
+        },
+      ],
+      lifecycleRules: [
+        {
+          abortIncompleteMultipartUploadAfter: Duration.days(90),
+          expiration: Duration.days(365),
+          transitions: [
+            {
+              storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+              transitionAfter: Duration.days(30),
+            },
+          ],
+        },
+      ],
+    })
   }
 }
