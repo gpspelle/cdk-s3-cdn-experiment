13cded0b3f1a97f087774c59e7b20ab4acf9051b
gpspelle <gpsunicamp016@gmail.com>
2022-05-01 15:55:45 -0300

    add cdn and add fetch cdn for SA - still need to fix cdk stack for US

--- utils/fetchFiles.js
+++ utils/fetchFiles.js
@@ -6,15 +6,16 @@ const {
 } = process.env;
 
 const AWS_BUCKET_NAME = `${AWS_REGION}-latency-test-pfg-unicamp.s3.${AWS_REGION}.amazonaws.com`
+const CUSTOM_DOMAIN = 'https://gpspelle.click';
 
 const fileSizes = [1, 10, 100, 1000, 10000];
 const mutex = new Mutex();
 
-const asyncCall = async (i, size) => {
+const asyncCall = async (i, size, domain) => {
     const release = await mutex.acquire();
     console.time(`[${i}] Get ${size}kb file`)
     await axios
-        .get(`https://${AWS_BUCKET_NAME}/${size}kb`)
+        .get(domain)
         .then(res => {
             console.timeEnd(`[${i}] Get ${size}kb file`);
             console.log(`statusCode: ${res.status}`);
@@ -28,9 +29,18 @@ const asyncCall = async (i, size) => {
         });
 }
 
-console.time('Total')
+console.time('Total without CDN')
 for (var i = 0; i < 10; i+=1) {
-    await asyncCall(i, fileSizes[2]);
+    const size = fileSizes[2];
+    await asyncCall(i, size, `https://${AWS_BUCKET_NAME}/${size}kb`);
 }
+console.timeEnd('Total without CDN')
 
-console.timeEnd('Total')
+console.time('Total with CDN')
+for (var i = 0; i < 10; i+=1) {
+    const size = fileSizes[2];
+    await asyncCall(i, size, `${CUSTOM_DOMAIN}/${size}kb`);
+}
+console.timeEnd('Total with CDN')
+
+
